820b249dd4756d01abf41102086107f6881f01eb
ktr <rohitht.2022@scis.smu.edu.sg>
2025-10-08 20:47:01 +0800

    changes to choose baby design

--- frontend/nylo_template/lib/resources/pages/choose_baby_page.dart
+++ frontend/nylo_template/lib/resources/pages/choose_baby_page.dart
@@ -5,9 +5,8 @@ import '/resources/pages/base_navigation_hub.dart';
 import '/config/keys.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
-
 class ChooseBabyPage extends StatefulWidget {
-  static RouteView path = ("/choose-baby", (context) => ChooseBabyPage());
+  static RouteView path = ("/choose-baby", (context) => const ChooseBabyPage());
 
   const ChooseBabyPage({Key? key}) : super(key: key);
 
@@ -35,30 +34,83 @@ class _ChooseBabyPageState extends State<ChooseBabyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Choose Baby")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Choose Your Baby",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
       body: SafeArea(
         child: _babies.isEmpty
             ? const Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: _babies.length,
-                itemBuilder: (context, index) {
-                  final baby = _babies[index];
-                  return ListTile(
-                    leading: Image.asset(
-                      'public/images/baby_icon_animated.png',
-                      width: 40,
-                      height: 40,
-                    ),
-                    title: Text(baby.name ?? 'Unnamed Baby'),
-                    subtitle: baby.dob != null
-                        ? Text("DOB: ${baby.dob!.toLocal().toIso8601String().split('T').first}")
-                        : null,
-                    onTap: () async {
-                      await Keys.selectedBabyId.save(baby.id);
-                      routeTo(BaseNavigationHub.path, navigationType: NavigationType.pushAndForgetAll);
-                    },
-                  );
-                },
+            : Padding(
+                padding: const EdgeInsets.all(20),
+                child: GridView.builder(
+                  itemCount: _babies.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.8,
+                    mainAxisSpacing: 24,
+                    crossAxisSpacing: 24,
+                  ),
+                  itemBuilder: (context, index) {
+                    final baby = _babies[index];
+                    return GestureDetector(
+                      onTap: () async {
+                        await Keys.selectedBabyId.save(baby.id);
+                        routeTo(BaseNavigationHub.path, navigationType: NavigationType.pushAndForgetAll);
+                      },
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: const DecorationImage(
+                                  image: AssetImage('public/images/baby_icon_animated.png'),
+                                  fit: BoxFit.cover,
+                                ),
+                                border: Border.all(color: Colors.grey.shade300, width: 2),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    blurRadius: 6,
+                                    offset: Offset(0, 3),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 12),
+                          Text(
+                            baby.name ?? "Unnamed",
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          if (baby.dob != null)
+                            Text(
+                              "DOB: ${baby.dob!.toLocal().toIso8601String().split('T').first}",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey[600],
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
       ),
     );
