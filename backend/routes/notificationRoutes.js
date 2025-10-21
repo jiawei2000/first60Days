5e88b8bff46b1fd81f12d8a4cd7b16b1c14adf68
Alastair Lim <alastair0852@gmail.com>
2025-10-21 19:09:12 +0800

    adjust notif route

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = express.Router()
 const notificationController = require('../controllers/notificationController')
 
-
-router.put('/:notificationId', notificationController.markRead);
+router.patch('/:notificationId', notificationController.markRead);
 router.delete('/:notificationId', notificationController.softDelete);
 
 module.exports = router
