f6641c67a61893d4e8769947c20b6fc669c7434b
Jiawei <jiawei.ang.2022@scis.smu.edu.sg>
2025-10-27 13:21:21 +0800

    navbar view role items

--- webapp/src/navigation/vertical/index.js
+++ webapp/src/navigation/vertical/index.js
@@ -1,23 +1,34 @@
 export default [
+    //Admin Pages
     {
         title: 'Home',
-        to: { name: 'root' },
+        to: { name: 'admin' },
         icon: { icon: 'bx-home-alt' },
+        viewBy: 'admin'
     },
     {
         title: 'Manage User',
         to: { name: 'admin-user-manage-user' },
         icon: { icon: 'bx-user' },
+        viewBy: 'admin'
     },
     {
         title: 'Manage Trainers',
         to: { name: 'admin-trainer-manage-trainer' },
         icon: { icon: 'bx-user' },
+        viewBy: 'admin'
     },
-
+    // Trainer Pages
+    {
+        title: 'Home',
+        to: { name: 'trainer' },
+        icon: { icon: 'bx-home-alt' },
+        viewBy: 'trainer'
+    },
     {
         title: 'Manage User',
         to: { name: 'trainer-user-manage-users' },
         icon: { icon: 'bx-user' },
+        viewBy: 'trainer'
     }
 ]
