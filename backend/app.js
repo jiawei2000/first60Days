8cb5e90142abe4bbd74c8d79896396bff41a5c63
Alastair Lim <alastair0852@gmail.com>
2025-11-08 11:52:08 +0800

    test deploy

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,14 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+
+// Load env variables
+dotenv.config()
+
 app.use(express.json({ limit: '10mb' })); // or higher if needed
+app.use(cors())
+// app.use(express.json())
+app.use(morgan('tiny'))
 
 const thresholdRoutes = require('./routes/thresholdRoutes');
 const adminRoutes = require('./routes/adminRoutes');
@@ -24,15 +31,6 @@ const excelRoutes = require('./routes/excelRoutes');
 // require('./jobs/topicNotifications');
 // require('./jobs/personalNotifications');
 // require('./jobs/processStatistics');
-
-// Load env variables
-dotenv.config()
-
-app.use(cors())
-// app.use(express.json())
-app.use(express.json({ limit: '10mb' })); // or higher if needed
-
-app.use(morgan('tiny'))
 
 // Define routes
 app.get('/', (req, res) => {
