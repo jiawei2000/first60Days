df5dcfed84da90515778e67510c0cad0eeb93cc7
Alastair Lim <alastair0852@gmail.com>
2025-11-20 16:06:25 +0800

    deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,40 +1,28 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const { initializeApp, applicationDefault, cert, getApps } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 const admin = require("firebase-admin");
 
 require('dotenv').config();
 
-const serviceAccount = {
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+if (!getApps().length) {
+  initializeApp(
+    process.env.FIREBASE_PRIVATE_KEY
+      ? { credential: cert({
+          type: process.env.FIREBASE_TYPE,
+          project_id: process.env.FIREBASE_PROJECT_ID,
+          private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          client_email: process.env.FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.FIREBASE_CLIENT_ID,
+          auth_uri: process.env.FIREBASE_AUTH_URI,
+          token_uri: process.env.FIREBASE_TOKEN_URI,
+          auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+          universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+        }) }
+      : { credential: applicationDefault() }     // Deployment
+  );
 }
 
-admin.initializeApp({
-    credential: cert(serviceAccount)
-});
-
 const db = getFirestore();
-
 module.exports = db;
-
-// test deploy, uncomment above for local or wtv
-// const { initializeApp, applicationDefault, getApps } = require('firebase-admin/app');
-// const { getFirestore } = require('firebase-admin/firestore');
-
-// if (!getApps().length) {
-//   initializeApp({
-//     credential: applicationDefault(),
-//   });
-// }
-
-// const db = getFirestore();
-// module.exports = db;
